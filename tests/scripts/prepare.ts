59234687a79f658f814e9ebfec991edad00fd3c8
Vadzim Sharai <vadzimsharai@gmail.com>
2025-11-25 23:46:22 +0000

    fix: remove import.meta.url from  tests prepare script

--- tests/scripts/prepare.ts
+++ tests/scripts/prepare.ts
@@ -1,9 +1,5 @@
 import * as path from 'path'
-import { fileURLToPath } from 'url'
 import { execSync } from 'child_process'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const sandboxTestName = 'sandbox.test.ts'
 const sandboxTestPath = path.join(__dirname, `../${sandboxTestName}`)
